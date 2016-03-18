7b6a2043b623d293759463c75880b96b857c0346
YengTsan <Yengttt@gmail.com>
2016-03-18 14:25:08 +0800

    QQ

---
+++ age.rb
@@ -0,0 +1,12 @@
+
+puts "How old are you"
+
+age=gets.chomp
+
+puts "In 10 years you will be: #{age}"
+age=age.to_i+10
+puts "In 20 years you will be: #{age}"
+age=age.to_i+10
+puts "In 10 years you will be: #{age}"
+age=age.to_i+10
+puts "In 10 years you will be: #{age}"
---
+++ name.rb
@@ -0,0 +1,17 @@
+puts "whats your first name?"
+
+first_name = gets.chomp
+
+puts "whats your last name?"
+
+last_name = gets.chomp
+
+name = first_name +" "+ last_name
+
+puts "Nice to meet you!" + name
+
+10.times do
+  
+  puts name
+end
+
